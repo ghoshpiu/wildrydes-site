ee998dec5d500cd61fd069cc54a9f4b2122f5608
ghoshpiu <92353385+ghoshpiu@users.noreply.github.com>
2025-02-11 02:19:44 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_72kRtOCgS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'lb85hd5f9vhm50mo20e1kuef6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
