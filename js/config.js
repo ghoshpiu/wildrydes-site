d6a4af09d58ec2dec9fe511ddccff445245a85ac
ghoshpiu <92353385+ghoshpiu@users.noreply.github.com>
2025-02-11 19:57:11 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_72kRtOCgS', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'lb85hd5f9vhm50mo20e1kuef6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_L8F1a5RkR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3h9ebjk2p0dent608lqqo4cqoo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
